09eec2a9abe7f745083590527427a885102c17d0
chandrainf <surel.chandrapratama@gmail.com>
2021-07-10 23:30:33 +0700

    Main/New Product

---
+++ frontend/src/pages/Main/NewProduct/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../../components/Navbar'
+import NewProducts from '../../../part/Product/NewProducts'
+
+function NewProduct() {
+  return (
+    <div>
+      <Navbar />
+      <NewProducts />
+    </div>
+  )
+}
+
+export default NewProduct
