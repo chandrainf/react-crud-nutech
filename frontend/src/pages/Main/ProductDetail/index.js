850019e10b036a778afc3c6ea3214541f230314e
chandrainf <surel.chandrapratama@gmail.com>
2021-07-10 23:31:31 +0700

    Main/Procuct Detail

---
+++ frontend/src/pages/Main/ProductDetail/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../../components/Navbar'
+import ProductDetails from '../../../part/Product/ProductDetails'
+
+function ProductDetail() {
+  return (
+    <div>
+      <Navbar />
+      <ProductDetails />
+    </div>
+  )
+}
+
+export default ProductDetail
