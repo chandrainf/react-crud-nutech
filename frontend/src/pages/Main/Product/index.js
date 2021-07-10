007fd2ddedb6edb067a3c2ac7d39bac1a5884732
chandrainf <surel.chandrapratama@gmail.com>
2021-07-10 23:31:02 +0700

    Main/Product

---
+++ frontend/src/pages/Main/Product/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../../../components/Navbar'
+import AllProduct from '../../../part/Product/AllProduct'
+
+function Product() {
+  return (
+    <div>
+      <Navbar />
+      <AllProduct />
+    </div>
+  )
+}
+
+export default Product
+
+
+
